72a24e844c99dfff375474a365242f4edf432045
kevin feng <kfeng631@gmail.com>
2018-07-03 18:25:21 +0800

    initial commit

---
+++ src/components/Contact.vue
@@ -0,0 +1,7 @@
+<template>
+<v-layout>this is contact</v-layout>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
---
+++ src/components/Home.vue
@@ -0,0 +1,181 @@
+<template>
+  <v-layout row wrap>
+     <v-flex xs4>
+       <v-card>
+         <v-card-title primary-title>
+            <div>
+              <div class="headline">Batman vs Superman</div>
+              <span class="grey--text">2016 ‧ Science fiction film/Action
+fiction ‧ 3h 3m</span>
+            </div>
+         </v-card-title>
+         <v-card-text>
+             It's been nearly two years since Superman's (Henry Cavill)
+colossal battle with Zod (Michael Shannon) devastated the city of
+Metropolis. The loss of life and collateral damage left many feeling angry
+and helpless, including crime-fighting billionaire Bruce Wayne (Ben
+Affleck). Convinced that Superman is now a threat to humanity, Batman
+embarks on a personal vendetta to end his reign on Earth, while the
+conniving Lex Luthor (Jesse Eisenberg) launches his own crusade against the
+Man of Steel.
+         </v-card-text>
+         <v-card-actions>
+            <v-btn flat color="purple">Rate this movie</v-btn>
+            <v-spacer></v-spacer>
+         </v-card-actions>
+       </v-card>
+     </v-flex>
+     <v-flex xs4>
+       <v-card>
+         <v-card-title primary-title>
+            <div>
+              <div class="headline">Logan</div>
+              <span class="grey--text">2017 ‧ Drama/Science fiction film ‧ 2h
+21m</span>
+            </div>
+         </v-card-title>
+         <v-card-text>
+             It's been nearly two years since Superman's (Henry Cavill)
+colossal battle with Zod (Michael Shannon) devastated the city of
+Metropolis. The loss of life and collateral damage left many feeling angry
+and helpless, including crime-fighting billionaire Bruce Wayne (Ben
+Affleck). Convinced that Superman is now a threat to humanity, Batman
+embarks on a personal vendetta to end his reign on Earth, while the
+conniving Lex Luthor (Jesse Eisenberg) launches his own crusade against the
+Man of Steel.
+         </v-card-text>
+         <v-card-actions>
+            <v-btn flat color="purple">Rate this movie</v-btn>
+            <v-spacer></v-spacer>
+         </v-card-actions>
+       </v-card>
+     </v-flex>
+     <v-flex xs4>
+       <v-card>
+         <v-card-title primary-title>
+            <div>
+              <div class="headline">Star Wars: The Last Jedi</div>
+              <span class="grey--text">2017 ‧ Fantasy/Science fiction film ‧
+2h 35m</span>
+            </div>
+         </v-card-title>
+         <v-card-text>
+             It's been nearly two years since Superman's (Henry Cavill)
+colossal battle with Zod (Michael Shannon) devastated the city of
+Metropolis. The loss of life and collateral damage left many feeling angry
+and helpless, including crime-fighting billionaire Bruce Wayne (Ben
+Affleck). Convinced that Superman is now a threat to humanity, Batman
+embarks on a personal vendetta to end his reign on Earth, while the
+conniving Lex Luthor (Jesse Eisenberg) launches his own crusade against the
+Man of Steel.
+         </v-card-text>
+         <v-card-actions>
+            <v-btn flat color="purple">Rate this movie</v-btn>
+            <v-spacer></v-spacer>
+         </v-card-actions>
+       </v-card>
+     </v-flex>
+     <v-flex xs4>
+       <v-card>
+         <v-card-title primary-title>
+            <div>
+              <div class="headline">Wonder Woman</div>
+              <span class="grey--text">2017 ‧ Fantasy/Science fiction film ‧
+2h 21m</span>
+            </div>
+         </v-card-title>
+         <v-card-text>
+             It's been nearly two years since Superman's (Henry Cavill)
+colossal battle with Zod (Michael Shannon) devastated the city of
+Metropolis. The loss of life and collateral damage left many feeling angry
+and helpless, including crime-fighting billionaire Bruce Wayne (Ben
+Affleck). Convinced that Superman is now a threat to humanity, Batman
+embarks on a personal vendetta to end his reign on Earth, while the
+conniving Lex Luthor (Jesse Eisenberg) launches his own crusade against the
+Man of Steel.
+         </v-card-text>
+         <v-card-actions>
+            <v-btn flat color="purple">Rate this movie</v-btn>
+            <v-spacer></v-spacer>
+         </v-card-actions>
+       </v-card>
+     </v-flex>
+     <v-flex xs4>
+       <v-card>
+         <v-card-title primary-title>
+            <div>
+              <div class="headline">Dunkirk</div>
+              <span class="grey--text">2017 ‧ Drama/Thriller ‧ 2 hours</span>
+            </div>
+         </v-card-title>
+         <v-card-text>
+             It's been nearly two years since Superman's (Henry Cavill)
+colossal battle with Zod (Michael Shannon) devastated the city of
+Metropolis. The loss of life and collateral damage left many feeling angry
+and helpless, including crime-fighting billionaire Bruce Wayne (Ben
+Affleck). Convinced that Superman is now a threat to humanity, Batman
+embarks on a personal vendetta to end his reign on Earth, while the
+conniving Lex Luthor (Jesse Eisenberg) launches his own crusade against the
+Man of Steel.
+         </v-card-text>
+         <v-card-actions>
+            <v-btn flat color="purple">Rate this movie</v-btn>
+            <v-spacer></v-spacer>
+         </v-card-actions>
+       </v-card>
+     </v-flex>
+     <v-flex xs4>
+       <v-card>
+         <v-card-title primary-title>
+            <div>
+              <div class="headline">The Revenant</div>
+              <span class="grey--text">2015 ‧ Drama/Thriller ‧ 2h 36m</span>
+            </div>
+         </v-card-title>
+         <v-card-text>
+             It's been nearly two years since Superman's (Henry Cavill)
+colossal battle with Zod (Michael Shannon) devastated the city of
+Metropolis. The loss of life and collateral damage left many feeling angry
+and helpless, including crime-fighting billionaire Bruce Wayne (Ben
+Affleck). Convinced that Superman is now a threat to humanity, Batman
+embarks on a personal vendetta to end his reign on Earth, while the
+conniving Lex Luthor (Jesse Eisenberg) launches his own crusade against the
+Man of Steel.
+         </v-card-text>
+         <v-card-actions>
+            <v-btn flat color="purple">Rate this movie</v-btn>
+            <v-spacer></v-spacer>
+         </v-card-actions>
+       </v-card>
+     </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
